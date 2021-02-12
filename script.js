b03efe28e8ff828621246a70c22526175891723e
John Kim <johnkimmsterr@gmail.com>
2021-02-12 15:00:36 -0500

    js commenting, housekeeping

--- script.js
+++ script.js
@@ -3,45 +3,52 @@ const generateBtn = document.getElementById("generate");
 const characterSliderRange = document.getElementById('characterSliderRange');
 const characterNumberRange = document.getElementById('characterNumberRange');
 
-// Arrays of letters, numbers and symbols created by the createArray function stored into constants using their ASCII code 
-// ASCII table reference provided by http://www.asciitable.com/
+// Arrays created referring to the characters charCodes 
+// ASCII table/charCode reference provided by http://www.asciitable.com/
 const uppercaseArray = createArray(65, 90);
 const lowercaseArray = createArray(97, 122);
 const numberArray = createArray(48, 57);
 const symbolArray = createArray(33, 47);
 
-// Function Created to Create arrays with specified values emcompassing the necessary characters by charCode
+// Create arrays using the charCode range as function arguments
 function createArray(min, max) {
     const newArray = [];
     for (i = min; i <= max; i++) {
         newArray.push(i);
-    }
+    };
     return newArray;
-}
+};
+
 // Event Listener for Character Slider, performs changeSliderNumber function after an input is detected on DOM elements #characterSliderRange and #characterNumberRange
 characterNumberRange.addEventListener('input', changeSliderNumber);
 characterSliderRange.addEventListener('input', changeSliderNumber);
-// defines changeSliderNumber function, synchronizes the slider and number display by setting both to a constant called sliderValue
-// target event property selects the element that triggered specific DOM event, in this case, adjusting the slider value.  
+// Defines changeSliderNumber function, synchronizes the slider and number display by setting both to a constant called sliderValue
+// Target event property selects the element that triggered specific DOM event, in this case, adjusting the slider value. 
+// Targeted element display value is changed to equal values on both elements to sync slider 
 function changeSliderNumber(slide) {
     const sliderValue = slide.target.value;
     characterNumberRange.value = sliderValue;
     characterSliderRange.value = sliderValue;
-}
+};
+
 // Event Listener for Generate Password Button
-// Researched arrow function syntax to shorthand the function called when the generateBtn element is clicked
+// Researched arrow function syntax to shorthand the function that is called when the generateBtn element is clicked
 // parseInt allows characterNumberRange's type to be a number instead of a string
-// Finalized password joins randomly selected indexes in the available array of characters 
+// Finalized password set to the value of the generatePassword with numberOfChars as an argument 
 // Randomly joined password is logged into the textarea containing final password 
 generateBtn.addEventListener("click", () => {
     let numberOfChars = parseInt(characterNumberRange.value, 10);
     let finalizedPassword = generatePassword(numberOfChars);
     document.getElementById('generatedPassword').value = finalizedPassword; 
-    console.log('finalPassword: ', finalizedPassword);
 });
 
 // Created function called when generateBtn is clicked. generatePassword takes argument numberOfChars, defined as parseInt(characterNumberRange.value)
-// if statements check to see which character arrays are to be added
+// First if alerts user to check at least one box if no boxes are checked 
+// If statements check to see which character arrays are to be added
+// Password to be generated stored as a new array. 
+// For loop iterates through available arrays (can tell if available through if(...checked))
+// Random available characters are pushed into randomizedPassword[] until its index reaches the specififed numberOfChars
+// Returns the newly joined password to be generated.  
 function generatePassword(numberOfChars) {
     let characterCodes = [];
     const uppercaseCheckElement = document.getElementById('includesUpperCase');
@@ -63,7 +70,7 @@ function generatePassword(numberOfChars) {
     if (symbolCheckElement.checked) {
         characterCodes = characterCodes.concat(symbolArray);
     }
-    let randomizedPassword = []
+    let randomizedPassword = [];
     for (var i = 0; i < numberOfChars; i++) {
         const randomIndex = Math.floor(Math.random() * characterCodes.length)
         const randomCharacter = characterCodes[randomIndex];
